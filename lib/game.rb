e5e6cdf854639b5c35bfea746fe37cb70fe94da6
drewjbartlett <drewb9@gmail.com>
2018-05-23 14:53:30 -0700

    Add passing test suite for rock paper scissors

--- lib/game.rb
+++ lib/game.rb
@@ -10,14 +10,44 @@ class Game
     self.started || false
   end
 
+  def finish
+    @started = false
+  end
+
+  def finished?
+    !self.started
+  end
+
   def play(left = nil, right = nil)
     raise 'Game must first be started' unless started?
+
     return nil unless (left && right)
-    if (left == :rock && right == :scissors)
-      "Rock beats scissors!"
+
+    game_result = ""
+
+    finish
+
+    if (left == right)
+      game_result = "Tie game. Try again!"
+
+      start
+    elsif (left == :rock && right == :scissors)
+      game_result = "Rock beats scissors!"
+    elsif (left == :scissors && right == :rock)
+      game_result = "Rock beats scissors!"
+    elsif (left == :paper && right == :rock)
+      game_result = "Paper beats rock!"
+    elsif (left == :rock && right == :paper)
+      game_result = "Paper beats rock!"
+    elsif (left == :scissors && right == :paper)
+      game_result = "Scissors vs paper!"
+    elsif (left == :paper && right == :scissors)
+      game_result = "Scissors vs paper!"
     else
-      :UNKNOWN
+      game_result = :UNKNOWN
     end
+
+    game_result
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,85 +1,113 @@
 require_relative '../lib/game'
 
+
 describe 'A game of rock paper scissors' do
+  let(:game) { Game.new }
+  let(:started_game) {
+    game = Game.new
+    game.start
+    game
+  }
 
   it 'exists' do
-    Game.new
+    :game
   end
 
   it 'can start' do
-    Game.new.start
+    game.start
   end
 
   it 'is not initially started' do
-    game = Game.new
     expect(game).to_not be_started
   end
 
   it 'a started game knows that it has started' do
-    game = Game.new
-    game.start
-    expect(game).to be_started
+    expect(started_game).to be_started
   end
 
   describe 'playing' do
 
     context 'without starting' do
       it 'can not be played if it has not started' do
-        game = Game.new
         expect { game.play }.to raise_error(RuntimeError)
       end
     end
 
     context 'after starting' do
       it 'can be played if it has started' do
-        game = Game.new
-        game.start
-        expect { game.play }.to_not raise_error
+        expect { started_game.play }.to_not raise_error
       end
 
       it 'returns nil when not provided with anything' do
-        game = Game.new
-        game.start
-        expect(game.play).to be_nil
+        expect(started_game.play).to be_nil
       end
 
       it 'returns nil when only provided one rock, paper or scissor' do
-        game = Game.new
-        game.start
-        expect(game.play(:FAKE)).to be_nil
+        expect(started_game.play(:FAKE)).to be_nil
       end
 
       it 'does not return nil when two objects are provided' do
-        game = Game.new
-        game.start
-        expect(game.play(:FAKE, :FAKE)).to_not be_nil
+        expect(started_game.play(:FAKE, :FAKE)).to_not be_nil
       end
     end
 
     context 'rock vs scissors' do
       it 'announces the correct winner' do
-        game = Game.new
+        expect(started_game.play(:rock, :scissors)).to eq("Rock beats scissors!")
+
         game.start
-        expect(game.play(:rock, :scissors)).to eq("Rock beats scissors!")
         expect(game.play(:scissors, :rock)).to eq("Rock beats scissors!")
       end
-      skip('is finished')
+
+      it 'will mark the game as finished' do
+        started_game.play(:rock, :scissors)
+
+        expect(started_game).to be_finished
+      end
     end
 
     context 'rock vs paper' do
-      skip('returns "Paper beats rock!"')
-      skip('is finished')
+      it 'announces the correct winner' do
+        expect(started_game.play(:paper, :rock)).to eq("Paper beats rock!")
+        
+        game.start
+        expect(game.play(:rock, :paper)).to eq("Paper beats rock!")
+      end
+
+      it 'will mark the game as finished' do
+        started_game.play(:rock, :paper)
+
+        expect(started_game).to be_finished
+      end
     end
 
-
     context 'scissors vs paper' do
-      skip('returns "Scissors vs paper!"')
-      skip('is finished')
+      it 'announces the correct winner' do
+        expect(started_game.play(:scissors, :paper)).to eq("Scissors vs paper!")
+
+        game.start
+        expect(game.play(:paper, :scissors)).to eq("Scissors vs paper!")
+      end
+
+      it 'will mark the game as finished' do
+        started_game.play(:scissors, :paper)
+
+        expect(started_game).to be_finished
+      end
     end
 
     context 'a tie game' do
-      skip('returns "Tie game. Try again!"')
-      skip('is not finished')
+      it 'announces the correct winner' do
+        expect(started_game.play(:rock, :rock)).to eq("Tie game. Try again!")
+        expect(started_game.play(:paper, :paper)).to eq("Tie game. Try again!")
+        expect(started_game.play(:scissors, :scissors)).to eq("Tie game. Try again!")
+      end
+
+      it 'will mark the game as finished' do
+        started_game.play(:scissors, :scissors)
+
+        expect(started_game).to_not be_finished
+      end
     end
 
   end
